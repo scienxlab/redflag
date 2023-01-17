8cbfaf81a911d263af08eafa4d15ab65cff05a75
Matt Hall <mtha@equinor.com>
2023-01-17 13:11:25 +0100

    modernize package build process
    This change removes setup.py and setup.cfg and relies solely
    on pyproject.toml. Also avoids a bug in scipy v1.10.0 which
    was causing tests to fail. Finally updates the CI to use the
    new, simpler pip options and add discrete docs build.
    
    Fixes #16

--- src/redflag/distributions.py
+++ src/redflag/distributions.py
@@ -18,7 +18,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from __future__ import annotations  # To support PEP 585 and PEP 604.
+from __future__ import annotations
 
 from typing import Optional, NamedTuple, Callable
 from collections import namedtuple
