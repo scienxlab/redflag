ea41b70b54675ca703ad0b7e7328b0cb364d14e6
kwinkunks <kwinkunks@gmail.com>
2023-09-20 18:26:50 +0200

    Continue w pandas accessors

--- src/redflag/pandas.py
+++ src/redflag/pandas.py
@@ -44,6 +44,18 @@ except:
     register_series_accessor = dummy_decorator
 
 
+TEMPLATES = {
+    'continuous': """
+Outliers:    {outliers}
+Correlated:  {correlated}
+Dummy scores:{dummy_regression_scores}
+""",
+    'categorical': """
+Imbalance degree: {imbalance_degree}
+Minority classes: {minority_classes}
+"""
+}
+
 @register_series_accessor("redflag")
 class SeriesAccessor:
     def __init__(self, pandas_obj):
@@ -72,17 +84,21 @@ class SeriesAccessor:
 
         return scores
 
-    def check(self, random_state=None):
+    def report(self, random_state=None):
         results = {}
         if is_continuous(self._obj):
             results['outliers'] = get_outliers(self._obj)
             results['correlated'] = is_correlated(self._obj)
             results['dummy_scores'] = dummy_regression_scores(self._obj)
-
+            template = TEMPLATES['continuous']
         else:
             # Categorical.
+            min_class = minority_classes(self._obj)
             results['imbalance'] = imbalance_degree(self._obj)
             results['dummy_scores'] = dummy_classification_scores(self._obj, random_state=random_state)
+            template = TEMPLATES['categorical']
+
+        return template.format(**results)
 
 
 @register_dataframe_accessor("redflag")
@@ -90,25 +106,12 @@ class DataFrameAccessor:
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
+    # Probably should not implement these target-only methods.
     def imbalance_degree(self, target=None):
-        if is_continuous(self._obj):
-            warnings.warn('The column does not seem categorical.')
-        return imbalance_degree(self._obj[target])
+        return self._obj[target].redflag.imbalance_degree()
 
     def minority_classes(self, target=None):
-        if is_continuous(self._obj):
-            warnings.warn('The column does not seem categorical.')
-        return minority_classes(self._obj[target])
+        return self._obj[target].redflag.minority_classes()
 
     def dummy_scores(self, target=None, task=None, random_state=None):
-        if task is None:
-            task = 'regression' if is_continuous(self._obj[target]) else 'classification'
-
-        if task == 'classification':
-            scores = dummy_classification_scores(self._obj[target], random_state=random_state)
-        elif task == 'regression':
-            scores = dummy_regression_scores(self._obj[target])
-        else:
-            raise ValueError("`task` must be 'classification' or 'regression', or None to decide automatically.")
-
-        return scores
+        return self._obj[target].redflag.dummy_scores(task=task, random_state=random_state)
